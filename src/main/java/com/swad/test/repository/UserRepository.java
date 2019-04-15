d3ccce866bbd07eb3356f5aa0ad00c07b2e58f6c
wangyong <vangyong@126.com>
2019-04-15 20:08:22 +0800

    修改jpa

--- src/main/java/com/swad/test/repository/UserRepository.java
+++ src/main/java/com/swad/test/repository/UserRepository.java
@@ -3,10 +3,13 @@ package com.swad.test.repository;
 import java.util.List;
 
 import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.swad.test.entity.User;
@@ -20,4 +23,21 @@ public interface UserRepository extends JpaRepository<User, Long>,JpaSpecificati
     @Cacheable(value = "getAllUsers",keyGenerator="keyGenerator2") 
     @Query(value = "SELECT a.* FROM tb_user a", nativeQuery = true)
     List<User> getAllUsers();
+    
+    @Cacheable(key ="#id")
+    @Query(value = "select * from user where id =?1",nativeQuery = true)
+    User findById(@Param("id") Long id);
+    
+    @CachePut(key = "#id")
+    @Query(value = "insert into tb_user(id,username,password) values(?1,?2,'123456')",nativeQuery = true)
+    void createById(@Param("id")Long id,@Param("name")String name);
+    
+    @CachePut(key = "#id")
+    @Query(value = "update tb_user set username=?2 where id=:?1",nativeQuery = true)
+    void updataById(Long id,String name);
+    
+   // @CacheEvict(key ="#id",allEntries=true)
+    @Query(value = "delete from tb_user where id=?1",nativeQuery = true)
+    void deleteById(Long id);
+    
 }
