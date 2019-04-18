74a650c99dc1d332cea2108fbb49495e7d6e0ac0
wangyong <vangyong@126.com>
2019-04-18 10:59:57 +0800

    修改对接kafka功能

--- src/main/java/com/swad/test/component/CsocLogsConsumerListener.java
+++ src/main/java/com/swad/test/component/CsocLogsConsumerListener.java
@@ -6,17 +6,20 @@ import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
-public class KafkaConsumerListener {
+public class CsocLogsConsumerListener {
+    
+    
 	/**
-	 * 实时获取kafka数据(生产一条，监听生产topic自动消费一条)
-	 * 
+	 * 日志监听器
 	 * @param record
 	 * @throws IOException
 	 */
 	@KafkaListener(topics = { "test" })
 	public void listen(ConsumerRecord<?, ?> record) throws IOException {
 		String value = (String) record.value();
-		System.out.println(value);
 		System.out.printf("topic = %s, offset = %d, value = %s \n", record.topic(), record.offset(), record.value());
+		
+		
+		
 	}
 }
---
+++ src/main/java/com/swad/test/service/AttackerService.java
@@ -0,0 +1,74 @@
+package com.swad.test.service;
+
+import java.math.BigInteger;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import javax.annotation.Resource;
+
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.ListOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+import com.swad.test.constant.CommonConstant;
+import com.swad.test.vo.CsocLogs;
+
+
+@Service
+public class AttackerService {
+    
+    @Resource 
+    private RedisTemplate redisTemplate;
+    
+    public void putAttackerStatistics(CsocLogs csocLogs) {
+     
+        
+    }
+    
+    public void putAttackerList(CsocLogs csocLogs) {
+        Map attackerMap = new HashMap();
+        attackerMap.put("src_ip", csocLogs.getSource_endpoint_ip());
+        attackerMap.put("src_geo_city", csocLogs.getSource_geo_city());
+        attackerMap.put("src_countryCode", csocLogs.getSource_geo_countryCode());
+        attackerMap.put("des_ip", csocLogs.getDestination_endpoint_ip());
+        attackerMap.put("des_geo_city", csocLogs.getDestination_geo_city());
+        attackerMap.put("des_countryCode", csocLogs.getDestination_geo_countryCode());
+        
+        //最近24小时的数据
+        String key24h = CommonConstant.ATTACKER_LIST+":"+"24h";
+        HashOperations hashOperations = redisTemplate.opsForHash();
+        Map key24hAttacker = (Map)hashOperations.get(key24h, attackerMap.get("src_ip"));
+        if(key24hAttacker!=null) {
+            key24hAttacker.put("logs_count", Integer.valueOf(((Map)key24hAttacker).get("logs_count").toString()).intValue()+1);
+            hashOperations.put(key24h, attackerMap.get("src_ip"), attackerMap);
+        }else {
+            hashOperations.put(key24h,attackerMap.get("src_ip"), attackerMap); 
+        }
+        redisTemplate.expire(key24h, 24, TimeUnit.HOURS);
+        
+        //30天的数据
+        String key7d = CommonConstant.ATTACKER_LIST+":"+"7d";
+        Map key7dAttacker = (Map)hashOperations.get(key24h, attackerMap.get("src_ip"));
+        if(key7dAttacker!=null) {
+            key7dAttacker.put("logs_count", Integer.valueOf(((Map)key7dAttacker).get("logs_count").toString()).intValue()+1);
+            hashOperations.put(key24h, attackerMap.get("src_ip"), attackerMap);
+        }else {
+            hashOperations.put(key24h,attackerMap.get("src_ip"), attackerMap); 
+        }
+        redisTemplate.expire(key7d, 7, TimeUnit.DAYS);
+        
+        //30天的数据
+        String key30d = CommonConstant.ATTACKER_LIST+":"+"30d";
+        Map key30dAttacker = (Map)hashOperations.get(key24h, attackerMap.get("src_ip"));
+        if(key30dAttacker!=null) {
+            key30dAttacker.put("logs_count", Integer.valueOf(((Map)key30dAttacker).get("logs_count").toString()).intValue()+1);
+            hashOperations.put(key24h, attackerMap.get("src_ip"), attackerMap);
+        }else {
+            hashOperations.put(key24h,attackerMap.get("src_ip"), attackerMap); 
+        }
+        redisTemplate.expire(key30d, 30, TimeUnit.DAYS);
+        
+    }
+}
