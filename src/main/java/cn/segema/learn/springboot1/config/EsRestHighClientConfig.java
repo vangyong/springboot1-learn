e8c8fed0213cb6f83317cfc918425cb53a9eb664
wangyong <vangyong@126.com>
2019-06-21 17:29:17 +0800

    修改es操作

---
+++ src/main/java/cn/segema/learn/springboot1/config/EsRestHighClientConfig.java
@@ -0,0 +1,41 @@
+package cn.segema.learn.springboot1.config;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.interceptor.KeyGenerator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Configuration
+public class EsRestHighClientConfig {
+    // 自定义缓存key生成策略
+    @Bean
+    public KeyGenerator keyGenerator() {
+        return new KeyGenerator() {
+            @Override
+            public Object generate(Object target, java.lang.reflect.Method method, Object... params) {
+                StringBuffer sb = new StringBuffer();
+                sb.append(target.getClass().getName());
+                sb.append(".");
+                sb.append(method.getName());
+                for (Object obj : params) {
+                    sb.append(obj.toString());
+                }
+                return sb.toString();
+            }
+        };
+    }
+    
+  
+}
--- src/main/java/cn/segema/learn/springboot1/controller/ElasticSearchController.java
+++ src/main/java/cn/segema/learn/springboot1/controller/ElasticSearchController.java
@@ -3,6 +3,16 @@ package cn.segema.learn.springboot1.controller;
 import java.io.IOException;
 import java.util.Date;
 
+import org.apache.http.HttpHost;
+import org.elasticsearch.action.ActionListener;
+import org.elasticsearch.action.admin.indices.alias.Alias;
+import org.elasticsearch.action.admin.indices.create.CreateIndexRequest;
+import org.elasticsearch.action.admin.indices.create.CreateIndexResponse;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.unit.TimeValue;
+import org.elasticsearch.common.xcontent.XContentType;
 import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.aggregations.AggregationBuilder;
@@ -29,6 +39,9 @@ public class ElasticSearchController {
 
     @Autowired
     private JestClient jestClient;
+    
+    private RestHighLevelClient restHighLevelClient = new RestHighLevelClient(
+        RestClient.builder(new HttpHost("localhost", 9200, "http")));
 
     @GetMapping("/test1/{statisticType}")
     public ResponseEntity test1(@PathVariable String  statisticType) {
@@ -81,4 +94,101 @@ public class ElasticSearchController {
         }
         return new ResponseEntity(searchResult, HttpStatus.OK);
     }
+    
+    
+    @GetMapping("/test3/{searchContent}")
+    public ResponseEntity test3(@PathVariable String  searchContent) throws IOException {
+        
+        CreateIndexRequest request = new CreateIndexRequest("twitter_two");//创建索引
+        //创建的每个索引都可以有与之关联的特定设置。
+        request.settings(Settings.builder()
+                .put("index.number_of_shards", 3)
+                .put("index.number_of_replicas", 1)
+        );
+        
+        //创建索引时创建文档类型映射
+        request.mapping("tweet",//类型定义
+                "  {\n" +
+                        "    \"tweet\": {\n" +
+                        "      \"properties\": {\n" +
+                        "        \"message\": {\n" +
+                        "          \"type\": \"text\"\n" +
+                        "        }\n" +
+                        "      }\n" +
+                        "    }\n" +
+                        "  }",//类型映射，需要的是一个JSON字符串
+                XContentType.JSON);
+      //为索引设置一个别名
+        request.alias(
+                new Alias("twitter_alias")
+        );
+        
+      //可选参数
+        request.timeout(TimeValue.timeValueMinutes(2));//超时,等待所有节点被确认(使用TimeValue方式)
+        //同步执行
+        CreateIndexResponse createIndexResponse = restHighLevelClient.indices().create(request);
+        
+        //异步方法不会阻塞并立即返回。
+        ActionListener<CreateIndexResponse> listener = new ActionListener<CreateIndexResponse>() {
+            @Override
+            public void onResponse(CreateIndexResponse createIndexResponse) {
+                //如果执行成功，则调用onResponse方法;
+            }
+            @Override
+            public void onFailure(Exception e) {
+                //如果失败，则调用onFailure方法。
+            }
+        };
+        restHighLevelClient.indices().createAsync(request, listener);//要执行的CreateIndexRequest和执行完成时要使用的ActionListener
+        
+        return new ResponseEntity(createIndexResponse, HttpStatus.OK);
+    }
+    
+
+    @GetMapping("/test4/{searchContent}")
+    public ResponseEntity test4(@PathVariable String  searchContent) throws IOException {
+        
+        CreateIndexRequest request = new CreateIndexRequest("twitter_four");//创建索引
+        //创建的每个索引都可以有与之关联的特定设置。
+        request.settings(Settings.builder()
+                .put("index.number_of_shards", 3)
+                .put("index.number_of_replicas", 1)
+        );
+        
+        //创建索引时创建文档类型映射
+        request.mapping("tweet",//类型定义
+                "  {\n" +
+                        "    \"tweet\": {\n" +
+                        "      \"properties\": {\n" +
+                        "        \"message\": {\n" +
+                        "          \"type\": \"text\"\n" +
+                        "        }\n" +
+                        "      }\n" +
+                        "    }\n" +
+                        "  }",//类型映射，需要的是一个JSON字符串
+                XContentType.JSON);
+        //为索引设置一个别名
+        request.alias(
+                new Alias("twitter_alias")
+        );
+        
+        request.timeout(TimeValue.timeValueMinutes(2));//超时,等待所有节点被确认(使用TimeValue方式)
+        
+        //异步方法不会阻塞并立即返回。
+        ActionListener<CreateIndexResponse> listener = new ActionListener<CreateIndexResponse>() {
+            @Override
+            public void onResponse(CreateIndexResponse createIndexResponse) {
+                //如果执行成功，则调用onResponse方法;
+            }
+            @Override
+            public void onFailure(Exception e) {
+                //如果失败，则调用onFailure方法。
+            }
+        };
+        restHighLevelClient.indices().createAsync(request, listener);//要执行的CreateIndexRequest和执行完成时要使用的ActionListener
+        
+        return new ResponseEntity("success", HttpStatus.OK);
+    }
+    
+    
 }
